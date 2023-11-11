8809467d71ba7b9e0a70e5d149fc724bd638c3f6
narrativityy <jake_crowley@comcast.net>
2023-11-10 17:30:38 -0800

    added home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-const Home = () => {
+const Home = (props) => {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/playlists')
+      .then(res => {
+        setPosts(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   return (
-    <div>Home</div>
+    <div className='d-flex justify-content-between align-items-center'>
+      {/* POSTS */}
+      <div className='d-flex justify-content-center align-items-center flex-wrap my-5 mx-4'>
+        {posts.map((elem) => {
+          return (
+            <div className='border p-3 rounded text-center' key={elem._id}>
+              <h5 className='border-bottom pb-2 px-2'>{elem.user.username}'s Top 10</h5>
+              <div className='pt-1'>
+                {elem.songs.map((song, i) => {
+                  return <p key={i}>{i + 1}. {song}</p>
+                })}
+              </div>
+            </div>
+          )
+        })}
+      </div>
+
+      {/* FRIENDS LIST */}
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
