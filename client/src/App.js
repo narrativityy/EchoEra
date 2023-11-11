e8d2f76ff47772a1e23deeeaaf54bb250c0f50f1
narrativityy <jake_crowley@comcast.net>
2023-11-10 16:31:25 -0800

    added styling, basic header, and home page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {Routes, Route, Link, Navigate} from 'react-router-dom'
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div className="d-flex justify-content-between align-items-center my-2 mx-3">
+        <h1>EchoEra</h1>
+        <div className="d-flex justify-content-center align-items-center">
+          <Link to="%" className='text-decoration-none'><h5 className="m-2 text-white">Home</h5></Link>
+          <Link to="%" className='text-decoration-none'><h5 className="m-2 text-white">Profile</h5></Link>
+          <Link to="%" className='text-decoration-none'><h5 className="m-2 text-white">Logout</h5></Link>
+        </div>
+      </div>
+
+      <Routes>
+        {/* MAIN REDIRECT */}
+        <Route path="/" element={<Navigate to='/home' />} />
+
+        {/* HOME */}
+        <Route path="home" element={<Home />} />
+
+      </Routes>
     </div>
   );
 }
